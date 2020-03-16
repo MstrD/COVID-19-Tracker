60bb312986d3439ce33bd87e82315d673dd14d05
MstrD <dadpmestre@gmail.com>
2020-03-16 15:37:48 +0000

    removal of unnecessary comments

--- scripts/line.js
+++ scripts/line.js
@@ -45,7 +45,6 @@ function buildLineChart(data) {
         width = window.innerWidth - margin.left - margin.right, // Use the window's width 
         height = 550 - margin.top - margin.bottom; // Use the window's height
 
-    //data = data.filter(d => d.confirmed !== 0);
     document.getElementById("country").innerText = country;
 
     var parseDate = d3.timeParse("%Y-%m-%d");
@@ -56,23 +55,19 @@ function buildLineChart(data) {
             labelsUsed.push(d.confirmed);
     })
 
-    // 5. X scale will use the index of our data
     var xScale = d3.scaleTime()
         .domain([parseDate(data[0].date), parseDate(data[data.length - 1].date)]) // input
         .range([0, width]); // output
 
-    // 6. Y scale will use the randomly generate number 
     var yScale = d3.scaleLinear()
         .domain([0, (d3.max(data, (d) => d.confirmed))+20]) // input 
         .range([height, 0]); // output 
 
-    // 7. d3's line generator
     var line = d3.line()
         .x(d => xScale(parseDate(d.date))) // set the x values for the line generator
         .y(d => yScale(d.confirmed)) // set the y values for the line generator 
         .curve(d3.curveMonotoneX); // apply smoothing to the line
     
-    // 1. Add the SVG to the page and employ #2
     if (!chartExists)
         var svg = d3.select("body").select("#line").append("svg")
             .attr("width", width + margin.left + margin.right)
@@ -82,14 +77,12 @@ function buildLineChart(data) {
     else
         var svg = d3.select("body").select("#line").select("svg");
 
-    // 3. Call the x axis in a group tag
     if (!chartExists) {
         svg.append("g")
             .attr("class", "x axis")
             .attr("transform", "translate(0," + height + ")")
             .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
 
-        // 4. Call the y axis in a group tag
         svg.append("g")
             .attr("class", "y axis")
             .call(d3.axisLeft(yScale))
@@ -129,7 +122,6 @@ function buildLineChart(data) {
         return function (t) { return i(t); };
     }
 
-    // 9. Append the path, bind the data, and call the line generator
     if (!chartExists) 
         svg.selectAll(".line")
             .data([data]) // 10. Binds data to the line
@@ -148,7 +140,6 @@ function buildLineChart(data) {
                 .duration(1000)
                 .attr("d", line);
 
-    // 12. Appends a circle for each datapoint 
     svg.selectAll(".dot")
         .data(chartExists ? chartData : data)
     .enter().append("circle") // Uses the enter().append() method
